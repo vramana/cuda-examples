d7c6ff520198a07811a6a42cd2ac11543a878d50
Ramana Venkata <vramana@users.noreply.github.com>
2025-12-07 09:27:13 +0530

    Print random ints

--- src/add_naive.cuh
+++ src/add_naive.cuh
@@ -25,9 +25,12 @@ void randomize_vector(float *v, int n) {
   gettimeofday(&time, nullptr);
   srand(time.tv_usec);
 
+  int rand_a = rand();
+  std::printf("rand: %d\n", rand_a);
+
   for (int i = 0; i < n; i++) {
     float tmp = float(rand() % 5) + 0.01 * (rand() % 5);
-    tmp = (rand() % 2 == 0) ? tmp : tmp * (1.0);
+    tmp = (rand() % 2 == 0) ? tmp : tmp * (-1.0);
     v[i] = n;
   }
 }
